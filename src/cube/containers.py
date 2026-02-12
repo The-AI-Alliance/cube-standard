1a620678ebd340d746b0b89259b60328cab8fff7
nicolas.gontier <nicolas.gontier@servicenow.com>
2026-02-12 13:25:10 -0500

    add container abstract class separation of concerns
    Signed-off-by: nicolas.gontier <nicolas.gontier@servicenow.com>

--- src/cube/containers.py
+++ src/cube/containers.py
@@ -1,12 +1,53 @@
+from abc import ABC, abstractmethod
+from typing import List
+
 from cube.types import TypedBaseModel
 
-# TODO: flesh out container management interfaces and implementations as we add support for containerized environments and tasks
 
-
-class Container(TypedBaseModel):
+class Container(ABC, TypedBaseModel):
     pass
 
 
-class ContainerConfig(TypedBaseModel):
-    def make(self) -> Container:
-        return Container()
+# Separate WHAT to run from HOW to run it.
+
+
+class ContainerConfig(ABC, TypedBaseModel):
+    """
+    WHAT to run. Owned by CUBE benchmark/task. Serializable (json)
+    Part of TaskConfig - retrieved via task_id
+    """
+
+    image: str
+    ram_gb: float
+    cpu_cores: float
+    gpu: bool = False
+    ports: List[int] | None = None
+
+    @abstractmethod
+    @staticmethod
+    def from_task_id(task_id: str) -> "ContainerConfig":
+        pass
+
+
+class ContainerBackend(ABC, TypedBaseModel):
+    """
+    HOW to run. Owned by harness users. Contain execution strategy (local,Modal,...).
+    Serializable (can pass to Ray workers)
+    User chooses which backend to use
+    """
+
+    @abstractmethod
+    def launch(self, conf: ContainerConfig) -> Container:
+        """Launch container from config. Blocks until ready."""
+        pass
+
+
+# Concrete implementations
+class LocalContainerBackend(ContainerBackend):
+    # TODO
+    pass
+
+
+class ModalContainerBackend(ContainerBackend):
+    # TODO
+    pass
