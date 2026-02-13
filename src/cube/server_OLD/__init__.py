c9e430fbabe81c645e3b2c8fb989ffc55f3a77af
nicolas.gontier <nicolas.gontier@servicenow.com>
2026-02-13 14:19:09 -0500

    remove old mcp server code
    Signed-off-by: nicolas.gontier <nicolas.gontier@servicenow.com>

--- src/cube/server_OLD/__init__.py
+++
@@ -1,8 +0,0 @@
-from cube.server_OLD.benchmark_server import create_benchmark_server_app
-from cube.server_OLD.task_server import SessionManager, create_task_server_app
-
-__all__ = [
-    "SessionManager",
-    "create_benchmark_server_app",
-    "create_task_server_app",
-]
