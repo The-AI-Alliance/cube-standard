96dbc7c0dac8e697d41ba82c3dc86b850aee12a7
Omar Younis <omar.younis98@gmail.com>
2026-03-23 22:53:29 +0100

    import fix

--- cube-resources/cube-chat/src/cube_chat/__init__.py
+++ cube-resources/cube-chat/src/cube_chat/__init__.py
@@ -1,5 +1,5 @@
 """cube-chat: concrete chat session implementations for cube-standard."""
 
-from cube_chat.basic_chat_session import BasicChatConfig, BasicChatSession
+from cube_chat.chat_session import BasicChatConfig, BasicChatSession
 
 __all__ = ["BasicChatConfig", "BasicChatSession"]
